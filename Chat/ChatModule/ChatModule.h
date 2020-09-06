748ee39e318a1954ae8b2c57a328cdfea4844838
Diego Atencia <diegoatencia96@gmail.com>
2020-09-06 17:42:29 -0300

    primer commit Chat

---
+++ Chat/ChatModule/ChatModule.h
@@ -0,0 +1,19 @@
+//
+//  ChatModule.h
+//  ChatModule
+//
+//  Created by Diego Atencia on 06/09/2020.
+//  Copyright © 2020 Diego Atencia. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ChatModule.
+FOUNDATION_EXPORT double ChatModuleVersionNumber;
+
+//! Project version string for ChatModule.
+FOUNDATION_EXPORT const unsigned char ChatModuleVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ChatModule/PublicHeader.h>
+
+
