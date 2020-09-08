64c29eb62220fc730b7004c29215fc4bc620333c
Diego Atencia <diegoatencia96@gmail.com>
2020-09-08 11:09:57 -0300

    nuevo modulo

--- Chat/Chat/ChatViewController.swift
+++ Chat/Chat/ChatViewController.swift
@@ -1,20 +1,23 @@
 //
-//  ViewController.swift
+//  ChatViewController.swift
 //  Chat
 //
-//  Created by Diego Atencia on 06/09/2020.
+//  Created by Diego Atencia on 08/09/2020.
 //  Copyright © 2020 Diego Atencia. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
+class ChatViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
         // Do any additional setup after loading the view.
     }
-
+    
+    func mostrarMensaje(){
+        print("Módulo chat")
+    }
 
 }
-
